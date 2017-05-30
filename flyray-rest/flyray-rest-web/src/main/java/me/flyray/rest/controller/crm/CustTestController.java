dc98ce47f4d7e2dd4bd3735326bbf5283ae42f72
boleixiongdi <923008946@qq.com>
2017-05-30 19:48:19 +0800

    fix rbac.properties名称

--- flyray-rest/flyray-rest-web/src/main/java/me/flyray/rest/controller/crm/CustTestController.java
+++ flyray-rest/flyray-rest-web/src/main/java/me/flyray/rest/controller/crm/CustTestController.java
@@ -1,13 +1,10 @@
 package me.flyray.rest.controller.crm;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-
-import me.flyray.crm.api.CustomerBaseService;
 
 @Controller
 public class CustTestController {
 
-	@Autowired
-	private CustomerBaseService customerBaseService;
+	/*@Autowired
+	private CustomerBaseService customerBaseService;*/
 }
