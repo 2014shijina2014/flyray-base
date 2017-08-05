3774e5d04cd0ca22fb546d308b0ef68c3add43c3
boleixiongdi <923008946@qq.com>
2017-08-05 10:00:00 +0800

    fix 不能注入 baseDao

--- flyray-crm/flyray-crm-service/src/main/java/me/flyray/crm/service/CustomerBillingServiceImpl.java
+++ flyray-crm/flyray-crm-service/src/main/java/me/flyray/crm/service/CustomerBillingServiceImpl.java
@@ -1,9 +1,13 @@
 package me.flyray.crm.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import me.flyray.common.service.AbstractBaseService;
 import me.flyray.crm.api.CustomerBillingService;
+import me.flyray.crm.dao.CustomerBillingDao;
 import me.flyray.crm.model.CustomerBilling;
 
 /** 
@@ -15,6 +19,11 @@ import me.flyray.crm.model.CustomerBilling;
 @Service("customerBillingService")
 public class CustomerBillingServiceImpl extends AbstractBaseService<CustomerBilling> implements CustomerBillingService{
 
+private static final Logger logger = LoggerFactory.getLogger(CustomerBillingServiceImpl.class);
+	
+	@Autowired
+	private CustomerBillingDao CustomerBillingDao;
+	
 	@Override
 	public void save(CustomerBilling customerBilling) {
 		// TODO Auto-generated method stub
