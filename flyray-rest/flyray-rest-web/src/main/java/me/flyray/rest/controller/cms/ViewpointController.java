3224a69d30063384a3dda2df04ddf104c6f9e88c
boleixiongdi <923008946@qq.com>
2017-08-19 14:02:37 +0800

    新增话题controller

---
+++ flyray-rest/flyray-rest-web/src/main/java/me/flyray/rest/controller/cms/ViewpointController.java
@@ -0,0 +1,16 @@
+package me.flyray.rest.controller.cms;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/** 
+* @author: bolei
+* @date：2017年8月19日 下午2:00:46
+* @description：首页圈子
+*/
+
+@Controller
+@RequestMapping("/api/cms/viewpoint")
+public class ViewpointController {
+
+}
