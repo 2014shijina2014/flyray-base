420fced6ccf162d1c8a054b15e30dd6b97934d61
centerroot <lfw6699@163.com>
2017-08-19 15:38:52 +0800

    新增话题请求

--- flyray-cms/flyray-cms-api/src/main/java/me/flyray/cms/model/Topic.java
+++ flyray-cms/flyray-cms-api/src/main/java/me/flyray/cms/model/Topic.java
@@ -1,11 +1,48 @@
 package me.flyray.cms.model;
 
-/** 
-* @author: bolei
-* @date：2017年3月6日 下午10:21:42 
-* @description：话题
-*/
+import java.util.Date;
+
+/**
+ * @author: bolei
+ * @date：2017年3月6日 下午10:21:42
+ * @description：话题
+ */
 
 public class Topic {
+	
+	/**
+	 * 话题标题
+	 */
+	private String title;
+	
+	/**
+	 * 话题内容
+	 */
+	private String content;
+	
+	/**
+	 * 描述 
+	 */
+	private String discription;
+	
+	/**
+	 * 背景图片
+	 */
+	private String img;
+	
+	/**
+	 * 发起话题人
+	 */
+	private String customerNo;
+	
+	/**
+	 * 发起时间
+	 */
+	private String createTime;
+	
+	/**
+	 * 审核删除标识
+	 */
+	private String flag;
 
 }
--- flyray-rest/flyray-rest-web/src/main/java/me/flyray/rest/controller/cms/TopicController.java
+++ flyray-rest/flyray-rest-web/src/main/java/me/flyray/rest/controller/cms/TopicController.java
@@ -1,7 +1,17 @@
 package me.flyray.rest.controller.cms;
 
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import me.flyray.pay.dto.CreateOrderRequst;
+import me.flyray.rest.util.ResponseHelper;
 
 /** 
 * @author: bolei
@@ -10,7 +20,24 @@ import org.springframework.web.bind.annotation.RestController;
 */
 
 @RestController
-@RequestMapping("/topic")
+@RequestMapping("/api/cms/topics")
 public class TopicController {
 
+	protected Logger logger = LoggerFactory.getLogger(getClass());
+	
+	/**
+	  * 创建支付交易订单
+	  * create
+	  */
+	@ResponseBody
+	@RequestMapping(value="/create", method = RequestMethod.POST)
+	public Map<String, Object> create(@RequestBody Map<String, String> param) {
+		logger.info("请求创建支付订单---start---{}",param);
+		CreateOrderRequst createOrder = new CreateOrderRequst();
+		createOrder.setCustomerNo((String)param.get("customerNo"));
+		createOrder.setMerchantNo((String)param.get("merchantNo"));
+		createOrder.setPayOrderNo((String)param.get("payOrderNo"));
+		//生成账单
+		return ResponseHelper.success(createOrder, "00", "创建支付订单成功");
+	}
 }
