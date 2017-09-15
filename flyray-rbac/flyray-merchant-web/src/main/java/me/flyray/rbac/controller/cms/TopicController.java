672df35742066ce054217323a29279c7584bc8b8
boleixiongdi <923008946@qq.com>
2017-09-15 21:53:36 +0800

    新增富文本编辑器

---
+++ flyray-rbac/flyray-merchant-web/src/main/java/me/flyray/rbac/controller/cms/TopicController.java
@@ -0,0 +1,18 @@
+package me.flyray.rbac.controller.cms;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import me.flyray.rbac.controller.AbstractController;
+
+/** 
+* @author: bolei
+* @date：2017年9月15日 下午8:50:20
+* @description：类说明 
+*/
+
+@RestController
+@RequestMapping("/cms/topic")
+public class TopicController extends AbstractController {
+
+}
