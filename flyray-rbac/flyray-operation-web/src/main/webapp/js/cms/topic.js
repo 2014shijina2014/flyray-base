280b551353ba1d0148efefa2c0f38a3468171f28
逆夜魂舞 <1136113717@qq.com>
2017-10-17 20:17:03 +0800

    评论bug修改

--- flyray-rbac/flyray-operation-web/src/main/webapp/js/cms/topic.js
+++ flyray-rbac/flyray-operation-web/src/main/webapp/js/cms/topic.js
@@ -52,7 +52,8 @@ var vm = new Vue({
 			title:null,
 			discription:null,
 			img:null
-		}
+		},
+		images:[]
 	},
 	methods: {
 		query: function () {
@@ -127,6 +128,60 @@ var vm = new Vue({
                 postData:{'title': vm.q.title},
                 page:page
             }).trigger("reloadGrid");
-		}
+		},
+		addPic: function (e) {
+            e.preventDefault();
+            $('input[type=file]').trigger('click');
+            return false;
+        },
+        onFileChange: function (e) {
+            var files = e.target.files || e.dataTransfer.files;
+            if (!files.length)return; 
+            console.log(files);
+            this.createImage(files);
+        },
+        createImage: function(file) {
+            if(typeof FileReader==='undefined'){
+                alert('您的浏览器不支持图片上传，请升级您的浏览器');
+                return false;
+            }
+            var image = new Image();         
+            var vm = this;
+            var leng=file.length;
+            for(var i=0;i<leng;i++){
+                var reader = new FileReader();
+                reader.readAsDataURL(file[i]); 
+                reader.onload =function(e){
+                  vm.images.push(e.target.result);                                    
+                };                 
+            }                        
+        },
+        delImage:function(index){
+            this.images.shift(index);
+        },
+        removeImage: function(e) {
+            this.images = [];
+        },
+        uploadImage: function() {
+            console.log(this.images);
+            return false;
+            var obj = {};
+            obj.images=this.images
+            $.ajax({
+                type: 'post',
+                url: "../cms/topic/save",
+                data: obj,
+                dataType: "json",
+                success: function(data) {
+                    if(data.status){
+                        alert(data.msg);
+                        return false;
+                    }else{
+                        alert(data.msg);
+                        return false;
+                    }
+                }
+            });
+        }
 	}
 });
